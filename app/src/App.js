b7d2a77854fea90100bee1ad14b2084f5902a3dd
adams66 <47400311+adams66@users.noreply.github.com>
2022-10-08 15:42:31 -0600

    start

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <div>
+      <Link to="/about">About</Link>
+      <h1>Home</h1>
+    </div>
+
+    
+  );
+}
+
+export default App;
---
+++ app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import About from './About';
+import {HashRouter, Routes, Route} from "react-router-dom";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <HashRouter>
+<Routes>
+<Route path="/" element={<App/>} />
+<Route path="/about" element={<About />}/>
+</Routes>
+  </HashRouter>
+  </React.StrictMode>
+);
+
