7b16fa30b8d0d64e293221e245bb5b6fdb296c89
adams66 <47400311+adams66@users.noreply.github.com>
2022-11-08 06:19:09 -0700

    Purshing from home

--- app/src/components/splash/Splash.js
+++ app/src/components/splash/Splash.js
@@ -3,21 +3,6 @@ import "./splash.css";
 
 function Splash(){
 return(
-<<<<<<< HEAD
-<div className=" pt-1 pb-1">
-    <div style = {{ height: "300px", backgroundColor: "purple"}} className="splash">
-
-
-    </div>
-
-<div className ="row p-0 m-0 mt-3 mb-3 g-3">
-    <div style={{backgroundColor: "red", height: "300px"}} className="col-12 col-md-4 p-0"></div>
-    <div style={{backgroundColor: "blue", height: "300px"}} className="col-12 col-md-4 p-0"></div>
-    <div style={{backgroundColor: "green", height: "300px"}} className="col-12 col-md-4 p-0"></div>
-</div>
-
-=======
->>>>>>> ae61bc35114c8f9234660b11a5d555a5e29f1b11
 
 
     <div style={{backgroundColor: "#231c4f"}} className="jumbotron m-3 p-3">
