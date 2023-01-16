416daab2cf0620431f0ff375d718f87f38e7a3f3
adams66 <47400311+adams66@users.noreply.github.com>
2023-01-15 18:24:35 -0700

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -15,29 +15,29 @@ self.addEventListener('install', function(event) {
   });
 
 
-  self.addEventListener('fetch', event => {
-    if (event.request.method === 'GET') {
-      let url = event.request.url.indexOf(self.location.origin) !== -1 ?
-        event.request.url.split(`${self.location.origin}/`)[1] :
-        event.request.url;
-      let isFileCached = $FILES.indexOf(url) !== -1;
+//   self.addEventListener('fetch', event => {
+//     if (event.request.method === 'GET') {
+//       let url = event.request.url.indexOf(self.location.origin) !== -1 ?
+//         event.request.url.split(`${self.location.origin}/`)[1] :
+//         event.request.url;
+//       let isFileCached = $FILES.indexOf(url) !== -1;
   
-      if (isFileCached) {
-        event.respondWith(
-          caches.open($CACHE_STORE)
-            .then(cache => {
-              return cache.match(url)
-                .then(response => {
-                  if (response) {
-                    return response;
-                  }
-                  throw Error('There is not response for such request', url);
-                });
-            })
-            .catch(error => {
-              return fetch(event.request);
-            })
-        );
-      }
-    }
-  });
+//       if (isFileCached) {
+//         event.respondWith(
+//           caches.open($CACHE_STORE)
+//             .then(cache => {
+//               return cache.match(url)
+//                 .then(response => {
+//                   if (response) {
+//                     return response;
+//                   }
+//                   throw Error('There is not response for such request', url);
+//                 });
+//             })
+//             .catch(error => {
+//               return fetch(event.request);
+//             })
+//         );
+//       }
+//     }
+//   });
