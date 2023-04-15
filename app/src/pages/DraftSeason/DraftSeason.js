610cac535b5b9dc946378f8ee5bc309750b7e3c4
adams66 <47400311+adams66@users.noreply.github.com>
2023-04-15 16:56:33 -0600

    Fixed Pagination

--- app/src/pages/DraftSeason/DraftSeason.js
+++ app/src/pages/DraftSeason/DraftSeason.js
@@ -1,3 +1,4 @@
+import * as helper from '../../helpers/helpers';
 import Key from '../../key/key.json';
 import React, {useEffect, useState} from "react";
 import "./draftSeason.css";
@@ -132,13 +133,13 @@ function Sdraft() {
                 <nav aria-label="Page navigation example ">
                     <ul className="pagination d-flex justify-content-center">
                         <li className="page-item m-1">
-                            <a  onClick={(event) => page(draft, round,"Previous")}  style={round == 1 ? {background:"red"} : null} className= {theme + " pageChanger btn  text-light"}
+                            <a  onClick={(event) => page(draft, round,"Previous")}  style={round == 1 ? {background:"red"} : {background:helper.themeColor(Key,'color')}} className= {theme + " pageChanger btn  text-light"}
                                 ><svg  xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
                               </svg></a>
                         </li>
                         <li  className="page-item m-1">
-                            <a  onClick={(event) => page(draft, round,"Next")}  style={round == draft.rounds ? {background:"red"} : null} className={theme + " pageChanger btn text-light"}
+                            <a  onClick={(event) => page(draft, round,"Next")}  style={round == draft.rounds ? {background:"red"} : {background:helper.themeColor(Key,'color')}} className={theme + " pageChanger btn text-light"}
                                 ><svg id="Next" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
                               </svg></a>
