913e1d3f74915e474e49efc7657e6a391e546243
adams66 <47400311+adams66@users.noreply.github.com>
2023-05-22 15:30:58 -0600

    the Start of the Single Teams

--- app/src/pages/NflTeamsSingle/NflTeamsSingle.js
+++ app/src/pages/NflTeamsSingle/NflTeamsSingle.js
@@ -2,23 +2,49 @@ import '../Home/home.css';
 import Key from '../../key/key.json';
 import  * as helper  from "../../helpers/helpers";
 import Foundation from '../../layout/Foundation';
+import { useEffect,useState } from 'react';
 
 
 
 
 function NflTeamsSingle(props) {
+	const [nflTeam, setNflTeam] = useState();
 
+
+	useEffect(()=>{
 	var url = window.location.pathname.split("/");
+	var team = url[2];
 
-	console.log(url);
+
+	fetch("https://homebase.dal-10.com/nfl/team/" + team)
+	.then((response)=>{ return response.json()})
+	.then((data)=>{setNflTeam(data)});
+
+		console.log(nflTeam);
+	},[])
+
+
+
 	
-
+if(nflTeam){
 
 	return (
 		<Foundation>
-                <h1 className='text-light text-center'>{url[2]}</h1>
+				<img style={{opacity: "1", display: "block", marginLeft: "auto", marginRight:"auto"}} src={"https://homebase.dal-10.com/nfl_logos/" + nflTeam[0].logo}  />
+				<h1 className='text-light text-center'>{"id " + nflTeam[0].id}</h1>
+				<h1 className='text-light text-center'>{"Conference " + nflTeam[0].conference}</h1>
+				<h1 className='text-light text-center'>{"Team" + nflTeam[0].team}</h1>
 		</Foundation>
 	);
+
+
+
+}
+
+
+
+	
+
 }
 
 export default NflTeamsSingle;
