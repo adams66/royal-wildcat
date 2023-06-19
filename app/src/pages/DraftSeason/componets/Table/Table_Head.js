c9a6d1585e1c010ac2926f28005a884f31b445de
adams66 <47400311+adams66@users.noreply.github.com>
2023-06-19 12:40:02 -0600

    More components for the table

---
+++ app/src/pages/DraftSeason/componets/Table/Table_Head.js
@@ -0,0 +1,39 @@
+function Table_Head() {
+    return (  
+
+        <thead>
+        <tr className="text-light">
+            <th class="text-center" scope="col">
+                Round
+            </th>
+            <th class="text-center" scope="col">
+                Round Number
+            </th>
+            <th class="text-center" scope="col">
+                Overall
+            </th>
+            <th class="text-center" scope="col">
+                Player Name
+            </th>
+            <th class="text-center" scope="col">
+                Fantasy Player
+            </th>
+            <th class="text-center" scope="col">
+                Position
+            </th>
+            <th class="text-center" scope="col">
+                NFL
+            </th>
+            <th class="text-center" scope="col">
+                College
+            </th>
+        </tr>
+    </thead>
+
+
+
+
+    );
+}
+
+export default Table_Head;
