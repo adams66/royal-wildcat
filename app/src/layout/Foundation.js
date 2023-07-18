2cdd8f5340086fa46ac5581908552d672888ad42
adams66 <47400311+adams66@users.noreply.github.com>
2023-07-17 20:25:43 -0600

    Theme color hamburger fix

--- app/src/layout/Foundation.js
+++ app/src/layout/Foundation.js
@@ -31,11 +31,15 @@ function Foundation(props) {
 	function hamburgerFunction(){
 		if(hamburger == 0){
 			setHamburger(1);
+			document.querySelector('meta[name="theme-color"]').setAttribute('content',  'rgb(16, 17, 22)');
+
 			setHamburgerClass("hamburger d-block d-lg-none clicked");
 			setSidebarSwitch("sidebar position-fixed w-100 d-flex flex-column justify-content-center d-lg-block");
 		}
 		else{
 			setHamburger(0);
+			document.querySelector('meta[name="theme-color"]').setAttribute('content',  'black');
+
 			setHamburgerClass("hamburger d-block d-lg-none");
 			setSidebarSwitch("sidebar position-fixed d-none d-lg-block");
 		}
