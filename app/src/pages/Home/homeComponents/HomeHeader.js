2207177f1eed02c4b5ecd97be4d7b16e36472797
adams66 <47400311+adams66@users.noreply.github.com>
2023-07-17 20:20:21 -0600

    Video inline fix

--- app/src/pages/Home/homeComponents/HomeHeader.js
+++ app/src/pages/Home/homeComponents/HomeHeader.js
@@ -8,7 +8,7 @@ function HomeHeader(props){
         <div style={{height: "500px", overflow:"hidden"}} className="row m-5 p-0 rounded">
             <div style={{height: "100%",position: "relative" }}  className="col p-0 m-0  rounded">
                 <div className="videoOverlay"></div>
-                <video className="video" style={{width: "100%"}} autoPlay muted loop src="./video.mp4" />
+                <video className="video" style={{width: "100%"}} autoPlay muted loop playsInline src="./video.mp4" />
                 <img class="header-logo" style={{position:"absolute", left:"50%", top:"50%", transform:"translate(-50%, -50%)",zIndex: 5, opacity:"0.9"}}  src="./logo.png" />
             </div>
         </div>
