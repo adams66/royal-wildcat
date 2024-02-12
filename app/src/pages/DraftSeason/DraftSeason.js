74840562d03cdab05f4700453b99e7a51ba4e19f
adams66 <47400311+adams66@users.noreply.github.com>
2024-02-12 11:37:24 -0700

    Clean up code

--- app/src/pages/DraftSeason/DraftSeason.js
+++ app/src/pages/DraftSeason/DraftSeason.js
@@ -61,108 +61,39 @@ function DraftSeason(props) {
 
 	function nflTeam(team) {
 		switch (team) {
-            case "0" :
-            return "";
-			case "1":
-				return 'https://homebase.dal-10.com/nfl_logos/arizona_cardinals.svg';
-		
-			case "2":
-				return 'https://homebase.dal-10.com/nfl_logos/atlanta_falcons.svg';
-		
-			case "3":
-				return 'https://homebase.dal-10.com/nfl_logos/carolina_panthers.svg';
-		
-			case "4":
-				return 'https://homebase.dal-10.com/nfl_logos/chicago_bears.svg';
-			
-			case "5":
-				return 'https://homebase.dal-10.com/nfl_logos/dallas_cowboys.svg';
-		
-			case "6":
-				return 'https://homebase.dal-10.com/nfl_logos/detriot_lions.svg';
-			
-			case "7":
-				return 'https://homebase.dal-10.com/nfl_logos/green_bay_packers.svg';
-		
-			case "8":
-             
-				return 'https://homebase.dal-10.com/nfl_logos/los_angeles_rams.svg';
-		
-			case "9":
-				return 'https://homebase.dal-10.com/nfl_logos/minnesota_vikings.svg';
-		
-			case "10":
-				return 'https://homebase.dal-10.com/nfl_logos/new_orleans_saints.svg';
-		
-			case "11":
-				return 'https://homebase.dal-10.com/nfl_logos/new_york_giants.svg';
-			
-			case "12":
-				return 'https://homebase.dal-10.com/nfl_logos/philadelphia_eagles.svg';
-		
-			case "13":
-				return 'https://homebase.dal-10.com/nfl_logos/san_francisco_49ers.svg';
-		
-			case "14":
-				return 'https://homebase.dal-10.com/nfl_logos/seattle_seahawks.svg';
-			
-			case "15":
-				return 'https://homebase.dal-10.com/nfl_logos/tampa_bay_buccaneers.svg';
-			
-			case "16":
-				return 'https://homebase.dal-10.com/nfl_logos/washington_commanders.svg';
-		
-			case "17":
-				return 'https://homebase.dal-10.com/nfl_logos/baltimore_ravens.svg';
-		
-			case "18":
-				return 'https://homebase.dal-10.com/nfl_logos/buffalo_bills.svg';
-		
-			case "19":
-				return 'https://homebase.dal-10.com/nfl_logos/cincinnati_bengals.svg';
-		
-			case "20":
-				return 'https://homebase.dal-10.com/nfl_logos/cleveland_browns.svg';
-			
-			case "21":
-				return 'https://homebase.dal-10.com/nfl_logos/denver_broncos.svg';
-			
-			case "22":
-				return 'https://homebase.dal-10.com/nfl_logos/houston_texans.svg';
-		
-			case "23":
-				return 'https://homebase.dal-10.com/nfl_logos/indianapolis_colts.svg';
-		
-			case "24":
-				return 'https://homebase.dal-10.com/nfl_logos/jacksonville_jaguars.svg';
-			
-			case "25":
-				return 'https://homebase.dal-10.com/nfl_logos/kansas_city_chiefs.svg';
-				break;
-			case "26":
-				return 'https://homebase.dal-10.com/nfl_logos/las_vegas_raiders.svg';
-		
-			case "27":
-				return 'https://homebase.dal-10.com/nfl_logos/miami_dolphins.svg';
-			
-			case "28":
-				return 'https://homebase.dal-10.com/nfl_logos/new_england_patriots.svg';
-			
-			case "29":
-				return 'https://homebase.dal-10.com/nfl_logos/new_york_jets.svg';
-			
-			case "30":
-				return 'https://homebase.dal-10.com/nfl_logos/pittsburgh_steeler.svg';
-			
-			case "31":
-				return 'https://homebase.dal-10.com/nfl_logos/tennessee_titans.svg';
-			
-
-			case "32":
-				return 'https://homebase.dal-10.com/nfl_logos/los_angeles_chargers.svg';
-			
-
-
+            case "0" :  return '';
+			case "1" :  return 'https://homebase.dal-10.com/nfl_logos/arizona_cardinals.svg';
+			case "2" :  return 'https://homebase.dal-10.com/nfl_logos/atlanta_falcons.svg';
+			case "3" :  return 'https://homebase.dal-10.com/nfl_logos/carolina_panthers.svg';
+			case "4" :  return 'https://homebase.dal-10.com/nfl_logos/chicago_bears.svg';
+			case "5" :  return 'https://homebase.dal-10.com/nfl_logos/dallas_cowboys.svg';
+			case "6" :  return 'https://homebase.dal-10.com/nfl_logos/detriot_lions.svg';
+			case "7" :  return 'https://homebase.dal-10.com/nfl_logos/green_bay_packers.svg';
+			case "8" :  return 'https://homebase.dal-10.com/nfl_logos/los_angeles_rams.svg';
+			case "9" :  return 'https://homebase.dal-10.com/nfl_logos/minnesota_vikings.svg';
+			case "10":  return 'https://homebase.dal-10.com/nfl_logos/new_orleans_saints.svg';
+			case "11":  return 'https://homebase.dal-10.com/nfl_logos/new_york_giants.svg';	
+			case "12":  return 'https://homebase.dal-10.com/nfl_logos/philadelphia_eagles.svg';
+			case "13":  return 'https://homebase.dal-10.com/nfl_logos/san_francisco_49ers.svg';
+			case "14":  return 'https://homebase.dal-10.com/nfl_logos/seattle_seahawks.svg';
+			case "15":  return 'https://homebase.dal-10.com/nfl_logos/tampa_bay_buccaneers.svg';
+			case "16":  return 'https://homebase.dal-10.com/nfl_logos/washington_commanders.svg';
+			case "17":  return 'https://homebase.dal-10.com/nfl_logos/baltimore_ravens.svg';
+			case "18":  return 'https://homebase.dal-10.com/nfl_logos/buffalo_bills.svg';
+			case "19":  return 'https://homebase.dal-10.com/nfl_logos/cincinnati_bengals.svg';
+			case "20":  return 'https://homebase.dal-10.com/nfl_logos/cleveland_browns.svg';
+			case "21":  return 'https://homebase.dal-10.com/nfl_logos/denver_broncos.svg';
+			case "22":  return 'https://homebase.dal-10.com/nfl_logos/houston_texans.svg';
+			case "23":  return 'https://homebase.dal-10.com/nfl_logos/indianapolis_colts.svg';
+			case "24":  return 'https://homebase.dal-10.com/nfl_logos/jacksonville_jaguars.svg';
+			case "25":  return 'https://homebase.dal-10.com/nfl_logos/kansas_city_chiefs.svg';
+			case "26":  return 'https://homebase.dal-10.com/nfl_logos/las_vegas_raiders.svg';
+			case "27":  return 'https://homebase.dal-10.com/nfl_logos/miami_dolphins.svg';
+			case "28":  return 'https://homebase.dal-10.com/nfl_logos/new_england_patriots.svg';
+			case "29":  return 'https://homebase.dal-10.com/nfl_logos/new_york_jets.svg';
+			case "30":  return 'https://homebase.dal-10.com/nfl_logos/pittsburgh_steeler.svg';
+			case "31":  return 'https://homebase.dal-10.com/nfl_logos/tennessee_titans.svg';
+			case "32":  return 'https://homebase.dal-10.com/nfl_logos/los_angeles_chargers.svg';
 		}
 	}
 
